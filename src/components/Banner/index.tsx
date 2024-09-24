412ecf11281051e1934c278ecd7395027c4e82e3
fwx5618177 <fengwenxuan2006@126.com>
2024-09-24 19:16:27 +0800

    chore: multiple languages

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,25 +2,28 @@ import React from "react";
 import Image from "next/image";
 import WalletButton from "@/components/WalletButton";
 import { useAccount } from "wagmi";
+import { useTranslation } from "react-i18next";
 
 const Banner = () => {
   const { isConnected } = useAccount();
+  const { t } = useTranslation("common");
 
   return (
     <section className="bg-bannerBg text-white w-full h-[300px] sm:h-[350px] md:h-[400px] lg:h-[450px] flex items-center">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between px-4 sm:px-8 w-full">
         <div className="flex-1 text-center md:text-left">
           <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold mb-2 sm:mb-4">
-            A powerful Web3 yield aggregator
+            {t("index-title")}
           </h2>
           <p className="text-sm sm:text-base md:text-lg text-secondary">
-            Maximize earnings with top-performing DeFi products curated from
-            multiple blockchain protocols
+            {t("index-desc")}
           </p>
 
-          {!isConnected && <div className="sm:hidden w-full flex items-center justify-center mt-2">
-            <WalletButton />
-          </div>}
+          {!isConnected && (
+            <div className="sm:hidden w-full flex items-center justify-center mt-2">
+              <WalletButton />
+            </div>
+          )}
         </div>
 
         {/* Image Container */}
---
+++ src/utils/languageList.ts
@@ -0,0 +1,1 @@
+export const languageList = ["en", "jp", "cn"];
