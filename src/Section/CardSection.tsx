99885652f492776b2d386df679d5eb35c6609543
fwx5618177 <fengwenxuan2006@126.com>
2024-09-24 16:25:53 +0800

    chore: add features

--- src/Section/CardSection.tsx
+++ src/Section/CardSection.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Card from "@/components/Card";
 import useStore from "@/store/useStore";
-import { mockProductList } from "@/mocks/mockProductList";
 
 const CardSection = () => {
-  const { productList } = useStore();
-  const products = productList?.length
-    ? productList.slice(0, 3)
-    : mockProductList;
+  const hardcodedProductList = [
+    {
+      abbrId: "1",
+      abbrLogo: "/path-to-usdt-logo.png",
+      abbrSubLogo: "/path-to-bsc-logo.png",
+      abbrTitle: "USDT",
+      abbrApy: "3285000",
+      abbrCycle: 3,
+      maturity: "2023-12-31",
+      tvl: "7.67M",
+      network: "bsc",
+      pid: 123,
+      contractAddress:
+        "0x6e7E9700350467bc645fb2985D6b63972Eb9F9e1" as `0x${string}`,
+      fixedDuration: 0,
+      startBlock: 1000000,
+      depositLimit: "$100,000",
+      abbrVersion: "",
+      abbrExpireTime: "",
+    },
+  ];
 
   return (
     <section className="py-6 px-4 sm:py-12 sm:px-8 bg-gray-100">
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 md:gap-8">
-        {products?.map((item, index) => (
+        {hardcodedProductList?.map((item, index) => (
           <Card
             abbrId={item?.abbrId}
             key={index + "_card"}
@@ -21,7 +37,7 @@ const CardSection = () => {
             abbrTitle={item.abbrTitle}
             abbrApy={item.abbrApy}
             abbrCycle={item.abbrCycle}
-            abbrVersion={item.abbrVersion}
+            abbrVersion={item?.abbrVersion}
             abbrExpireTime={String(item?.abbrExpireTime)}
             pid={item.pid}
             contractAddress={item.contractAddress as `0x${string}`}
